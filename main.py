01f6c25f654d3e4cb0f2478c76e812de963c2de5
khilesh007 <khilesh2907@gmail.com>
2022-06-28 17:30:05 +0530

    update

--- main.py
+++ main.py
@@ -31,5 +31,21 @@ def predict():
             'index.html',
             prediction_text='prediction value is {}'.format(prediction))
 
+@app.route("/api/predict", methods=["POST"])
+def apiPredict():
+    if "year" not in request.get_json() or "month" not in request.get_json():
+        return {"Error": "Year and Month are required!"}, 400
+
+    data = request.get_json()
+    year = data['year']
+    month = data['month']
+
+    if type(year) != int or month not in range(1, 13):
+        return {"Error": "Year and Month must be a valid numbers!"}, 400
+    else:
+        features = [[year, month]]
+        prediction = int(model.predict(features)[0])
+        return {'prediction': prediction}
+
 if __name__ == "__main__":
     app.run(debug=True)
